9af1d13b526c60295a99e942a300625824d9fded
Murakami <murakami.inform@gmail.com>
2024-02-02 02:01:35 +0900

    aaaa

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+namespace HakuShiro_DomainDriven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
